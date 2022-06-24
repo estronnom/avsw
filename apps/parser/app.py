47c4a51737a8a29fee362bd572a688606b1f06f4
estronnom <estronnom@gmail.com>
2022-06-25 02:22:42 +0300

    parser crud

--- apps/parser/app.py
+++ apps/parser/app.py
@@ -18,6 +18,27 @@ db = Database(host=MYSQL_HOST,
               user=MYSQL_USER,
               password=MYSQL_PASSWORD,
               database=MYSQL_DATABASE)
+db.execute(
+    "CREATE TABLE IF NOT EXISTS words ("
+    "word_id INT PRIMARY KEY AUTO_INCREMENT, "
+    "word VARCHAR(255) UNIQUE NOT NULL, "
+    "counter INT"
+    ");"
+)
+db.execute(
+    "CREATE TABLE IF NOT EXISTS files ("
+    "file_id INT PRIMARY KEY AUTO_INCREMENT, "
+    "file_name VARCHAR(255) UNIQUE NOT NULL"
+    ");"
+)
+db.execute(
+    "CREATE TABLE IF NOT EXISTS word_file ("
+    "word_id INT NOT NULL, "
+    "file_id INT NOT NULL,"
+    "FOREIGN KEY (word_id) REFERENCES words(word_id),"
+    "FOREIGN KEY (file_id) REFERENCES files(file_id)"
+    ");"
+)
 
 connection = get_pika_connection(RABBITMQ_HOST)
 channel = connection.channel()
@@ -25,8 +46,28 @@ channel.queue_declare(PARSER_QUEUE, durable=True)
 channel.queue_declare(READER_QUEUE, durable=True)
 
 
-def insert_words(words_dict):
-    pass
+def insert_words(words_dict, file_name):
+    db.execute(
+        "INSERT IGNORE INTO files(file_name)"
+        "VALUES (%s)",
+        (file_name,)
+    )
+    for word, counter in words_dict.items():
+        db.execute(
+            "INSERT INTO words(word, counter) "
+            "VALUES (%s, %s)"
+            "ON DUPLICATE KEY UPDATE counter=counter+%s",
+            (word, counter, counter)
+        )
+        db.execute(
+            "INSERT INTO word_file(word_id, file_id)"
+            "SELECT"
+            "(SELECT word_id FROM words"
+            " WHERE word=%s),"
+            "(SELECT file_id FROM files WHERE"
+            " file_name=%s)",
+            (word, file_name)
+        )
 
 
 def parse_file(path):
@@ -35,7 +76,10 @@ def parse_file(path):
         with open(path, 'r') as fh:
             for line in fh:
                 line = line.strip().replace('\xa0', '')
-                line = re.findall(r'\s[a-zA-Zа-яА-Я][a-zA-Zа-яА-Я]+\s', line)
+                line = re.findall(
+                    r'[a-zA-Zа-яА-Я][a-zA-Zа-яА-Я]+',
+                    line
+                )
                 for word in line:
                     word = word.lower()
                     words_dict[word] += 1
@@ -53,11 +97,14 @@ def callback(ch, method, properties, body):
     body_decoded = json.loads(body.decode())
     path = body_decoded["path"]
     words_dict = parse_file(path)
-    print(words_dict)
-    if words_dict and insert_words(words_dict):
-        if publish_message(channel):
+    file_name = os.path.split(path)[-1]
+    if words_dict:
+        insert_words(words_dict, file_name)
+        body = {
+            "file_name": f"{file_name}"
+        }
+        if publish_message(channel, READER_QUEUE, body):
             channel.basic_ack(delivery_tag=method.delivery_tag)
-            print(words_dict)
             print('Parser acknowledged message')
 
 
